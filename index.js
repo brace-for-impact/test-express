acdecfe1f5c9acc34a8abeba84c8e68e28a8bb9c
Selva Ganesh M <selva.ganesh@ogi-voos.com>
2025-05-31 16:35:33 +0530

    upg

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send(`Hello from ${process.env.NODE_ENV} environment!`);
+  res.send(`Hello from ${process.env.NODE_ENV} environment!. CI is working!`);
 });
 
 const PORT = process.env.PORT || 3000;
